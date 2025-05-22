dc780ad29d2d4aab3e9afd2da351ce8d4b73042a
PranavGharde <pranavgharde32@gmail.com>
2025-05-22 19:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	  event.preventDefault();
+	  const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+	   alert(`${firstName} ${lastName}`);
 
 }
